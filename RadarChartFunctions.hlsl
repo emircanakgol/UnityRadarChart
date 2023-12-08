48a54e54ff428c4f47731e1eb036f238c7093879
Emir Can Akgöl <emircanakgol@gmail.com>
2023-12-08 15:23:11 +0300

    Update RadarChartFunctions.hlsl
    Made some fields "const"

--- RadarChartFunctions.hlsl
+++ RadarChartFunctions.hlsl
@@ -1,12 +1,12 @@
 
 float calculatePolygon(float2 UV, float Sides, float Size)
 {
-    float pi = 3.14159265359;
+    const float pi = 3.14159265359;
     float aSize = Size * cos(pi / Sides);
     float2 uv = (UV * 2 - 1) / float2(aSize, aSize);
     uv.y *= -1;
-    float pCoord = atan2(uv.x, uv.y);
-    float r = 2 * pi / Sides;
+    const float pCoord = atan2(uv.x, uv.y);
+    const float r = 2 * pi / Sides;
     float distance = cos(floor(0.5 + pCoord / r) * r - pCoord) * length(uv);
 
     #if defined(SHADER_STAGE_RAY_TRACING)
@@ -17,40 +17,41 @@ float calculatePolygon(float2 UV, float Sides, float Size)
 }
 
 float segment(float2 p, float2 a, float2 b, float t) {
-    float2 ab = b - a, ap = p - a;
-    float ln = length(ap - ab * clamp(dot(ab, ap) / dot(ab, ab), 0.0, 1.0)) > t ? 1 : 0;
+    const float2 ab = b - a;
+    const float2 ap = p - a;
+    const float ln = length(ap - ab * clamp(dot(ab, ap) / dot(ab, ab), 0.0, 1.0)) > t ? 1 : 0;
     return 1 - ln;
 }
 
 float polygonLine(float2 UV, float Sides, float Size, float Thickness){
-    float polygonExpand = calculatePolygon(UV, Sides, Size + Thickness / 2);
-    float polygonDilate = calculatePolygon(UV, Sides, Size - Thickness / 2);
+    const float polygonExpand = calculatePolygon(UV, Sides, Size + Thickness / 2);
+    const float polygonDilate = calculatePolygon(UV, Sides, Size - Thickness / 2);
     return polygonExpand - polygonDilate;
 }
 
 void calculate_Polygon_float(float2 UV, float Sides, float Size, float OuterThickness, float InnerThickness, float Count, float4 LineColor, float4 BackgroundColor, out float4 Out)
 {
     float result = 0;
-    float sizeStep = Size/Count;
+    const float sizeStep = Size/Count;
     
     for(int i = 1; i <= Count; i++){
-        float polygon = polygonLine(UV, Sides, i * sizeStep, InnerThickness);
+        const float polygon = polygonLine(UV, Sides, i * sizeStep, InnerThickness);
         result += polygon;
     }
-    float outerPolygon = polygonLine(UV, Sides, Size, OuterThickness);
+    const float outerPolygon = polygonLine(UV, Sides, Size, OuterThickness);
     result = saturate(result + outerPolygon);
-    float neglectedArea = calculatePolygon(UV, Sides, Size);
-    float invertedResult = 1 - saturate(result);
-    float4 background = BackgroundColor * invertedResult * neglectedArea;
+    const float neglectedArea = calculatePolygon(UV, Sides, Size);
+    const float invertedResult = 1 - saturate(result);
+    const float4 background = BackgroundColor * invertedResult * neglectedArea;
     
-    float4 foreground = result * LineColor;
+    const float4 foreground = result * LineColor;
     Out = background + foreground;
 }
 void draw_Custom_Polygon_float(float2 UV, float Sides, float Size, float Count, float Thickness, UnityTexture2D Values, UnitySamplerState SS, out float4 Out)
 {   
-    float pi = 3.14159265359;
+    const float pi = 3.14159265359;
     float result = 0;
-    float2 uv = (UV * 2 - 1) / float2(Size, Size);
+    const float2 uv = (UV * 2 - 1) / float2(Size, Size);
     
     float values[100];
     values[0] = SAMPLE_TEXTURE2D(Values, SS, float2(0,0.5)).x;
@@ -60,11 +61,11 @@ void draw_Custom_Polygon_float(float2 UV, float Sides, float Size, float Count, 
         }      
     }
 
-    float angleIncrement = (2.0 * pi) / Sides;
-    float angleOffset = (Sides - 2) * pi/Sides / 2;
+    const float angleIncrement = (2.0 * pi) / Sides;
+    const float angleOffset = (Sides - 2) * pi/Sides / 2;
     float firstXPos = values[0] * cos(-angleOffset);
     float firstYPos = values[0] * sin(-angleOffset);
-    float2 firstPosition = float2(firstXPos,firstYPos);
+    const float2 firstPosition = float2(firstXPos,firstYPos);
     float2 previousPosition = float2(firstXPos,firstYPos);
     for(int j = 1; j < Sides; j++){
         float xPos = values[j] * cos(j * angleIncrement - angleOffset);
